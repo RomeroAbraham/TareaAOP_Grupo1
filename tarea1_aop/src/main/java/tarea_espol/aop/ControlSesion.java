06abc3586fcb73dc192259a334be28e842309a3c
AllanAcosta1404 <darkdemon1404@gmail.com>
2026-06-07 19:50:15 -0500

    Sección C terminada - AOP

--- tarea1_aop/src/main/java/tarea_espol/aop/ControlSesion.java
+++ tarea1_aop/src/main/java/tarea_espol/aop/ControlSesion.java
@@ -1,7 +1,20 @@
 package tarea_espol.aop;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+public class ControlSesion {
+    
+    private String estadoSesion;
+
+    public ControlSesion() {
+        System.out.println("Ejecutando constructor de ControlSesion...");
+        this.estadoSesion = "Sesión Activa";
+    }
+
+    public String getEstadoSesion() {
+        return estadoSesion;
+    }
+
+    public void setEstadoSesion(String estadoSesion) {
+        this.estadoSesion = estadoSesion;
     }
 }
